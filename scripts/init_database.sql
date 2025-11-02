2fe453c25d8f2a9d14e7852176433a3443175aad
ToyibOtus <toyibotusanya@gmail.com>
2025-11-02 15:29:52 +0000

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -6,9 +6,6 @@ Script Purpose:
 	This script creates a new database named "Datawarehouse". It checks the existence of this database,
 	deletes it if it exists, and then recreates a new one. Additionally, it also creates three schemas:
 	bronze, gold, and layer. 
-	To drop existing schemas in the database and recreate new ones with the same name,
-	this script also checks for the existence of existing & non-existing schemas, deletes them if
-	they exist, and recreates them.
 
 Warning:
 	Running this script deletes the existing database, "Datawarehouse", if it exists.
@@ -38,30 +35,14 @@ CREATE DATABASE Datawarehouse;
 USE Datawarehouse;
 GO
 
--- Drop Schema 'bronze' if it exist
-IF EXISTS (SELECT * FROM sys.schemas WHERE name = 'bronze')
-BEGIN
-	DROP SCHEMA bronze
-END;
-GO
 -- Create Schema 'bronze'
 CREATE SCHEMA bronze;
 GO
--- Drop Schema 'silver' if it exist
-IF EXISTS (SELECT * FROM sys.schemas WHERE name = 'silver')
-BEGIN
-	DROP SCHEMA silver
-END;
-GO
+
 -- Create Schema 'silver'
 CREATE SCHEMA silver;
 GO
--- Drop Schema 'gold' if it exist
-IF EXISTS (SELECT * FROM sys.schemas WHERE name = 'gold')
-BEGIN
-	DROP SCHEMA gold
-END;
-GO
+	
 -- Create Schema 'gold'
 CREATE SCHEMA gold;
 GO
