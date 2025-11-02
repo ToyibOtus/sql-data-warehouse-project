d89ee90fd27e25eb0da2b9f4fc78489f847190ca
ToyibOtus <toyibotusanya@gmail.com>
2025-11-02 16:56:21 +0000

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -19,7 +19,7 @@ Warning:
 USE master;
 GO
 -- Drop Database 'Datawarehouse' if it exixts
-IF EXISTS (SELECT * FROM sys.databases WHERE name = 'Datawarehouse')
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'Datawarehouse')
 BEGIN
 	ALTER DATABASE Datawarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE
 	DROP DATABASE Datawarehouse
