825fd5a2d13db218d6c11d5194c020526d20ecbf
ToyibOtus <toyibotusanya@gmail.com>
2025-11-02 08:56:02 +0000

    Refactor table column names in ddl_script.sql

--- scripts/bronze/ddl_script.sql
+++ scripts/bronze/ddl_script.sql
@@ -1,31 +1,46 @@
 /*
-==============================================================================================
-DDL Script: Create Bronze Tables
-==============================================================================================
-Script Purpose: 
-	This script creates tables in the 'bronze' schema, dropping existing tables
-	if they exist.
-	Run this script to re-define the structure of your 'bronze' tables
-  ==============================================================================================
+=======================================================================================================
+DDL Script: Create bronze tables
+=======================================================================================================
+Script Purpose:
+	This script checks the existence of the followning tables:
+		* crm_cust_info,
+		* crm_prd_info,
+		* crm_sales_details
+		* erp_cust_az12,
+		* erp_loc_a101, and
+		* erp_px_cat_g1v2,
+	deletes them if they exist, and recreates them.
+
+	Run this script to redefine the structure of your 'bronze' tables
+=======================================================================================================
 */
+USE MyDatawarehouse;
+GO
 
+-- Checks the existence of bronze table 'bronze.crm_cust_info'
 IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+-- Drops bronze table 'bronze.crm_cust_info'
 DROP TABLE bronze.crm_cust_info;
-
+GO
+-- Creates bronze table 'bronze.crm_cust_info'
 CREATE TABLE bronze.crm_cust_info
 (
 	cst_id INT,
 	cst_key NVARCHAR(50),
-	cst_firstname NVARCHAR(50),
-	cst_lastname NVARCHAR(50),
-	cst_maritalstatus NVARCHAR(50),
+	cst_first_name NVARCHAR(50),
+	cst_last_name NVARCHAR(50),
+	cst_marital_status NVARCHAR(50),
 	cst_gndr NVARCHAR(50),
 	cst_create_date DATE
 );
 
+-- Checks the existence of bronze table 'bronze.crm_prd_info'
 IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+-- Drops bronze table 'bronze.crm_prd_info'
 DROP TABLE bronze.crm_prd_info;
-
+GO
+-- Creates bronze table 'bronze.crm_prd_info'
 CREATE TABLE bronze.crm_prd_info
 (
 	prd_id INT,
@@ -33,29 +48,35 @@ CREATE TABLE bronze.crm_prd_info
 	prd_nm NVARCHAR(50),
 	prd_cost INT,
 	prd_line NVARCHAR(50),
-	prd_start_dt DATETIME,
-	prd_end_dt DATETIME
+	prd_start_dt DATE,
+	prd_end_dt DATE
 );
 
+-- Checks the existence of bronze table 'bronze.crm_sales_details'
 IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+-- Drops bronze table 'bronze.crm_sales_details'
 DROP TABLE bronze.crm_sales_details;
-
+GO
+-- Creates bronze table 'bronze.crm_sales_details'
 CREATE TABLE bronze.crm_sales_details
 (
 	sls_ord_num NVARCHAR(50),
 	sls_prd_key NVARCHAR(50),
 	sls_cust_id INT,
 	sls_order_dt INT,
-	sls_ship_dt INT,
-	sls_due_dt INT,
+	sls_ship_date INT,
+	sls_due_date INT,
 	sls_sales INT,
 	sls_quantity INT,
 	sls_price INT
 );
 
+-- Checks the existence of bronze table 'bronze.erp_cust_az12'
 IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+-- Drops bronze table 'bronze.erp_cust_az12'
 DROP TABLE bronze.erp_cust_az12;
-
+GO
+-- Creates bronze table 'bronze.erp_cust_az12'
 CREATE TABLE bronze.erp_cust_az12
 (
 	cid NVARCHAR(50),
@@ -63,22 +84,28 @@ CREATE TABLE bronze.erp_cust_az12
 	gen NVARCHAR(50)
 );
 
+-- Checks the existence of bronze table 'bronze.erp_loc_a101'
 IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
+-- Drops bronze table 'bronze.erp_loc_a101'
 DROP TABLE bronze.erp_loc_a101;
-
+GO
+-- Creates bronze table 'bronze.erp_loc_a101'
 CREATE TABLE bronze.erp_loc_a101
 (
 	cid NVARCHAR(50),
 	cntry NVARCHAR(50)
 );
 
+-- Checks the existence of bronze table 'bronze.erp_px_cat_g1v2'
 IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+-- Drops bronze table 'bronze.erp_px_cat_g1v2'
 DROP TABLE bronze.erp_px_cat_g1v2;
-
+GO
+-- Creates bronze table 'bronze.erp_px_cat_g1v2'
 CREATE TABLE bronze.erp_px_cat_g1v2
 (
 	id NVARCHAR(50),
 	cat NVARCHAR(50),
-	subcate NVARCHAR(50),
+	subcat NVARCHAR(50),
 	maintenance NVARCHAR(50)
 );
