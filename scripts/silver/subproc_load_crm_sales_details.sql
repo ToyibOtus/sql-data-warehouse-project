aaf918a908460dfc47f1f056df855c95aaacbce5
ToyibOtus <toyibotusanya@gmail.com>
2025-11-05 10:40:36 +0000

    Create subproc_load_crm_sales_details.sql

---
+++ scripts/silver/subproc_load_crm_sales_details.sql
@@ -0,0 +1,169 @@
+/*
+==================================================================================================================
+Stored Procedure: Load silver.load_crm_sales_details (Bronze -> Silver)
+==================================================================================================================
+Script Purpose:
+	This script loads silver.load_crm_sales_details from the source table bronze.crm_sales_details.
+	It performs appropriate data transformations, such as data cleansing, data enrinchment, data standardization
+	and normalization on the bronze table before loading the silver, ensuring adequate data quality.
+	Additionally, it also loads vital execution details into the etl log table 'audit.etl_log', ensuring
+	easy monitoring, traceability, and debugging.
+
+Parameter:
+	@run_id UNIQUEIDENTIFIER
+
+Usage:
+	EXEC silver.load_crm_sales_details @run_id = NEWID();
+
+Note:
+	It is recommended to execute the master procedure 'etl.run_master_pipeline' instead of individual
+    sub-procedures for the following reasons:
+        * It executes the entire ETL process in the correct sequence.
+        * It assigns the same @run_id across all sub-procedures for unified tracking.
+        * It provides centralized logging and easier monitoring.
+==================================================================================================================
+*/
+CREATE OR ALTER PROCEDURE silver.load_crm_sales_details @run_id UNIQUEIDENTIFIER AS
+BEGIN
+	-- Declare variables
+	DECLARE
+	@process_name NVARCHAR(50),
+	@start_time DATETIME,
+	@end_time DATETIME,
+	@rows_source INT,
+	@rows_loaded INT,
+	@source_table NVARCHAR(50),
+	@target_table NVARCHAR(50);
+
+	BEGIN TRY
+		-- Map values to variables
+		SET @process_name = 'Load silver.crm_sales_details';
+		SET @start_time = GETDATE();
+
+		-- Clear target table before load
+		TRUNCATE TABLE silver.crm_sales_details;
+
+		-- Load and transform data
+		INSERT INTO silver.crm_sales_details
+		(
+			sls_ord_num,
+			sls_prd_key,
+			sls_cust_id,
+			sls_order_dt,
+			sls_ship_dt,
+			sls_due_dt,
+			sls_sales,
+			sls_quantity,
+			sls_price
+		)
+		SELECT
+			TRIM(sls_ord_num) AS sls_ord_num,
+			TRIM(sls_prd_key) AS sls_prd_key,
+			sls_cust_id,
+			CASE 
+				WHEN sls_order_dt <= 0 OR LEN(sls_order_dt) <> 8 THEN NULL 
+				ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+			END sls_order_dt,
+			CASE 
+				WHEN sls_ship_dt <= 0 OR LEN(sls_ship_dt) <> 8 THEN NULL 
+				ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+			END sls_ship_dt,
+			CASE 
+				WHEN sls_due_dt <= 0 OR LEN(sls_due_dt) <> 8 THEN NULL 
+				ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+			END sls_due_dt,
+			CASE
+				WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales <> sls_quantity * sls_price THEN ABS(sls_quantity * sls_price)
+				ELSE sls_sales
+			END sls_sales,
+			sls_quantity,
+			CASE
+				WHEN sls_price IS NULL OR sls_price <= 0 THEN sls_sales/sls_quantity
+				ELSE sls_price
+			END sls_price
+		FROM bronze.crm_sales_details;
+
+		-- Map values to variables
+		SET @end_time = GETDATE();
+		SET @source_table = 'bronze.crm_sales_details';
+		SET @target_table = 'silver.crm_sales_details';
+		SELECT @rows_source = COUNT(*) FROM bronze.crm_sales_details;
+		SELECT @rows_loaded = COUNT(*) FROM silver.crm_sales_details;
+
+		-- Log success
+		INSERT INTO audit.etl_log
+		(
+			run_id,
+			layer,
+			load_type,
+			process_name,
+			load_status,
+			start_time,
+			end_time,
+			load_duration_seconds,
+			rows_source,
+			rows_loaded,
+			source_table,
+			target_table
+		)
+		VALUES
+		(
+			@run_id,
+			'silver',
+			'Full',
+			@process_name,
+			'Success',
+			@start_time,
+			@end_time,
+			DATEDIFF(second, @start_time, @end_time),
+			@rows_source,
+			@rows_loaded,
+			@source_table,
+			@target_table
+		);
+	END TRY
+
+	BEGIN CATCH
+		
+		-- Log any failure with error details
+		INSERT INTO audit.etl_log
+		(
+			run_id,
+			layer,
+			load_type,
+			process_name,
+			load_status,
+			start_time,
+			end_time,
+			load_duration_seconds,
+			source_table,
+			target_table,
+			error_number,
+			error_message,
+			error_line,
+			error_severity,
+			error_state,
+			error_procedure
+		)
+		VALUES
+		(
+			@run_id,
+			'silver',
+			'Full',
+			@process_name,
+			'Failure',
+			@start_time,
+			@end_time,
+			DATEDIFF(second, @start_time, @end_time),
+			@source_table,
+			@target_table,
+			ERROR_NUMBER(),
+			ERROR_MESSAGE(),
+			ERROR_LINE(),
+			ERROR_SEVERITY(),
+			ERROR_STATE(),
+			ERROR_PROCEDURE()
+		);
+	END CATCH
+END;
+GO
