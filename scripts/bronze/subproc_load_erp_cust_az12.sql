09b40b2b618744c1b4231fe9caee142aa99b7f2b
ToyibOtus <toyibotusanya@gmail.com>
2025-11-05 11:07:11 +0000

    Update subproc_load_erp_cust_az12.sql

--- scripts/bronze/subproc_load_erp_cust_az12.sql
+++ scripts/bronze/subproc_load_erp_cust_az12.sql
@@ -8,10 +8,11 @@ Script Purpose:
 	2. Uses the bulk insert command to load data from the source system into the bronze table.
 	3. Records important ETL execution details into the log table 'audit.etl_log'.
 
+	Additionally, it also loads vital execution details into the etl log table 'audit.etl_log', ensuring
+	easy monitoring, traceability, and debugging.
+
 Parameters:
 	@run_id UNIQUEIDENTIFIER
-	This procedure requires you input the value 'NEWID()' into the parameter if it is being run manually, 
-	and it doesn't return any values.
 
 Usage:
 	EXEC bronze.load_erp_cust_az12 @run_id = NEWID();
@@ -27,6 +28,7 @@ Notes:
 
 CREATE OR ALTER PROCEDURE bronze.load_erp_cust_az12 @run_id UNIQUEIDENTIFIER AS
 BEGIN
+	-- Declare variables
 	DECLARE 
 	@process_name NVARCHAR(50),
 	@start_time DATETIME,
@@ -35,17 +37,15 @@ BEGIN
 	@source_file NVARCHAR(50),
 	@file_path NVARCHAR(250);
 
-	-- ------------------------------------------------------------------
-	-- Checking For Error
-	-- ------------------------------------------------------------------
 	BEGIN TRY
+		-- Map values to variables
 		SET @process_name = 'Load bronze.erp_cust_az12';
 		SET @start_time = GETDATE();
 
-		-- Truncate Table 'bronze.erp_cust_az12'
+		-- Clear target table before load
 		TRUNCATE TABLE bronze.erp_cust_az12;
 
-		-- Bulk Insert Table 'bronze.erp_cust_az12'
+		-- Load Data
 		BULK INSERT bronze.erp_cust_az12
 		FROM 'C:\Users\PC\Documents\SQL_DataWareHouseProject\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
 		WITH
@@ -54,12 +54,13 @@ BEGIN
 			FIELDTERMINATOR = ',',
 			TABLOCK
 		);
+		-- Map values to variables
 		SET @end_time = GETDATE();
 		SET @source_file = 'erp_cust_az12';
 		SET @file_path = 'C:\Users\PC\Documents\SQL_DataWareHouseProject\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv';
 		SELECT @rows_loaded = COUNT(*) FROM bronze.erp_cust_az12;
 
-		-- Insert Into ETL Log Table 'audit.etl_log'
+		-- Log success
 		INSERT INTO audit.etl_log 
 		(
 			run_id, 
@@ -89,11 +90,9 @@ BEGIN
 			@file_path
 		);
 	END TRY
-	-- ------------------------------------------------------------------
-	-- Error Handling
-	-- ------------------------------------------------------------------
+	
 	BEGIN CATCH
-	-- Insert the following when an error occurs
+	-- Log any failure with error details
 		INSERT INTO audit.etl_log
 		(
 			run_id,
