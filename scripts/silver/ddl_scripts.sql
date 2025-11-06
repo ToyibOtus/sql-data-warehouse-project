5d810e26a738334e9766d08db9f4b4f8da2499aa
ToyibOtus <toyibotusanya@gmail.com>
2025-11-06 12:15:34 +0000

    Update ddl_scripts.sql

--- scripts/silver/ddl_scripts.sql
+++ scripts/silver/ddl_scripts.sql
@@ -84,7 +84,7 @@ CREATE TABLE silver.erp_px_cat_g1v2
 (
 	id NVARCHAR(50),
 	cat NVARCHAR(50),
-	subcate NVARCHAR(50),
+	subcat NVARCHAR(50),
 	maintenance NVARCHAR(50),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 )
